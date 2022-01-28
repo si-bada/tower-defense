4cab8d72c453c3f6ae76ab41f06c69d6114f0544
si-bada <bacharbhouri@gmail.com>
2022-01-28 10:13:35 +0100

    - Map - Enemy Spawning - Enemy Following Path

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int pathPointIndex = 0;
+
+
+    private void Start()
+    {
+        target = PathPoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            UpdatePathPoint();
+        }
+    }
+
+    private void UpdatePathPoint()
+    {
+        if(pathPointIndex >= PathPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        pathPointIndex++;
+        target = PathPoints.points[pathPointIndex];
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform EnemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves;
+
+    public TextMeshProUGUI waveCountDownText;
+
+    private float countDown = 2f;
+
+    private int waveNumber = 1;
+
+
+    private void Update()
+    {
+        if(countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+        waveCountDownText.text = Mathf.Round(countDown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+
+        for (int i=0; i<waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
