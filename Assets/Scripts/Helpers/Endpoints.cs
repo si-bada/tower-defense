481a671bf61aef90f11419be6f2b355061000fb6
si-bada <bacharbhouri@gmail.com>
2022-02-03 00:14:38 +0100

    Changing BaseURL to Localhost

--- Assets/Scripts/Helpers/Endpoints.cs
+++ Assets/Scripts/Helpers/Endpoints.cs
@@ -1,6 +1,6 @@
 ﻿public class Endpoints
 {
-    public static string baseURL = "http://18.119.86.3:3310/api/";
+    public static string baseURL = "localhost:8000/api/";
     public static string Signup = "Signup";
     public static string SaveScore = "Save";
     public static string Login = "Login";
